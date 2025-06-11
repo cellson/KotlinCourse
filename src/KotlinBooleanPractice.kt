dafc53fa48b8a1c10d529709d6f5577dddbc212e
cellson7170 <chris@solsticehoodriver.com>
2025-06-10 18:22:37 -0700

    chore: finish KotlinBooleanPractice.kt

---
+++ src/KotlinBooleanPractice.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+    // Boolean Data Type
+    var cookiePolicyAccepted: Boolean = true
+
+    // equal to
+    println("A" == "A") // true
+    println("A" == "B") // false
+    println(10 == 10) // true
+    println(20 == 10) // false
+
+    // not equal to
+    println("A" != "A") // false
+    println("A" != "B") // true
+    println(10 != 10) // false
+    println(20 != 10) // true
+
+    // greater than
+    println(10 > 10) // false
+    println(20 > 10) // true
+    println(10 > 20) // false
+
+    // less than
+    println(10 < 10) // false
+    println(20 < 10) // false
+    println(10 < 20) // true
+
+    // greater than or equal to
+    println(10 >= 10) // true
+    println(20 >= 10) // true
+    println(10 >= 20) // false
+
+    // less than or equal to
+    println(10 <= 10) // true
+    println(20 <= 10) // false
+    println(10 <= 20) // true
+
+}
